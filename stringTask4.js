627a9456d2f47f64236693ed14fbead4c52b1c2e
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-31 18:39:18 +0600

    push

--- stringTask4.js
+++ stringTask4.js
@@ -1,18 +1,18 @@
 // Task-5:
 // Capitalize Every first Letter of each word in a String
 
-let myTexts = "amar desh bangladesh ami amar desh k valobashi"
-let splitKorbo = myTexts.split(" ")
+let myTexts = "amar desh bangladesh ami amar desh k valobashi.The diligent student diligently pursued knowledge, striving to expand their understanding of the world.";
+let splitKorbo = myTexts.split(" ");
 // console.log(splitKorbo);
 let myStoreRoom = [];
 
 for (let i = 0; i < splitKorbo.length; i++) {
-let myVariable = splitKorbo[i];
-let capitalizedWord =myVariable.charAt(0).toUpperCase() + myVariable.slice(1);
-myStoreRoom.push(capitalizedWord)
-// console.log(myStoreRoom);
-    
-    
+  let myVariable = splitKorbo[i];
+  let capitalizedWord =
+    myVariable.charAt(0).toUpperCase() + myVariable.slice(1);
+    //   console.log(capitalizedWord);
+    myStoreRoom.push(capitalizedWord)
+    // console.log(myStoreRoom);
 }
-let myResult = myStoreRoom.join(' ');
+let myResult = myStoreRoom.join(" ");
 console.log(myResult);
