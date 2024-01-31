b4f9d07bd32ec371c98ec6efcc39846e5aba5ee5
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-31 23:29:23 +0600

    task

--- task6.js
+++ task6.js
@@ -1,21 +1,38 @@
 // let str = "Mississippi";
 // console.log(str.indexOf("i",3));
 
-const myObject = {
-    a: 1,
-    b: 2,
-    c: 3
-  };
-  
-  for (let prop in myObject) {
-    // console.log(myObject[prop]);
-  }
-  const x = ""
-if(!x.length){
+// const myObject = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+//   };
+
+//   for (let prop in myObject) {
+//     console.log( `${prop} ${myObject[prop]}`);
+//   }
+
+//   const x = ""
+// if(!x.length){
 // console.log("Hey")
-}else{
+// }else{
 // console.log("Hi")
+// }
+// const str = "Hello";
+// str[0] = "h";
+// console.log(str);
+
+const numbers = [23, 43, 2, 5, 346, 203, 21];
+let sumOdd = 0;
+let sumEven = 0;
+for (let i = 0; i < numbers.length; i++) {
+  const x = numbers[i];
+  // console.log(x);
+  if (x % 2 === 0) {
+    sumEven = sumEven + x;
+    // console.log(sumEven);
+  } else {
+    sumOdd = sumOdd + x;
+  }
 }
-const str = "Hello";
-str[0] = "h";
-console.log(str);
+console.log(sumEven);
+console.log(sumOdd);
