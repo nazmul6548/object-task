91abe4b3348438879d781ca2911dbbdda7159d0e
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-31 12:10:33 +0600

    object

---
+++ task4.js
@@ -0,0 +1,22 @@
+// Task-4
+// Count the number of properties.
+
+// Input:
+
+// let student = {
+//     name: 'Ariana Grande',
+//     age: 21,
+//     city: 'Gaibandha',
+//     isStudent: true
+// };
+// ans
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+}
+let numberOfProperty =Object.keys(student).length;
+console.log(numberOfProperty);
+
+// object properties theke number ber korte hole mean koita number property ace ta ber korar jonne Object.keys method bebohar kora lage.
